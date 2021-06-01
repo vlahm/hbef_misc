bfadc2dabebe260e9057cf233518346cb181e1a1
Mike Vlah <vlahm13@gmail.com>
2021-06-01 14:58:13 -0600

    removed archive, theorycond, datetime cols

--- edi_upload/edi_upload_prep.R
+++ edi_upload/edi_upload_prep.R
@@ -45,10 +45,10 @@ p = bind_rows(pc, ph) %>%
     select(site, date, timeEST, pH, DIC, spCond, temp, ANC960, ANCMet,
            precipCatch, fieldCode, notes, uniqueID, waterYr, datetime, Ca, Mg,
            K, Na, TMAl, OMAl, Al_ICP, NH4, SO4, NO3, Cl, PO4, DOC, TDN, DON,
-           SiO2, Mn, Fe, `F`, cationCharge, anionCharge, theoryCond, ionError,
-           duplicate, sampleType, ionBalance, canonical, pHmetrohm, archived)
+           SiO2, Mn, Fe, `F`, cationCharge, anionCharge, ionError,
+           duplicate, sampleType, ionBalance, canonical, pHmetrohm)
 
-#stream ####
+# stream ####
 
 sc = read.csv('~/HubbardBrook_weekly_stream_chemistry_curr.csv',
               stringsAsFactors = FALSE,
@@ -83,17 +83,52 @@ s = bind_rows(sc, sh) %>%
            hydroGraph, flowGageHt, fieldCode, notes, uniqueID, waterYr,
            datetime, Ca, Mg, K, Na, TMAl, OMAl, Al_ICP, NH4, SO4, NO3, Cl, PO4,
            DOC, TDN, DON, SiO2, Mn, Fe, `F`, cationCharge, anionCharge,
-           theoryCond, ionError, duplicate, sampleType, ionBalance, canonical,
-           pHmetrohm, archived)
+           ionError, duplicate, sampleType, ionBalance, canonical,
+           pHmetrohm)
 # gsub('\\t', ', ', strng)
 
 # out ####
 
+p$datetime = NULL
+s$datetime = NULL
 write_csv(p, '~/git/hbef/hbef_misc/edi_upload/HubbardBrook_weekly_precipitation_chemistry.csv')
 write_csv(s, '~/git/hbef/hbef_misc/edi_upload/HubbardBrook_weekly_stream_chemistry.csv')
+
 # zz = read.csv('/tmp/mozilla_mike0/HubbardBrook_weekly_precipitation_chemistry.csv',
 #               stringsAsFactors = FALSE)
 # table(nchar(zz$datetime))
 # zz2 = read.csv('/tmp/mozilla_mike0/HubbardBrook_weekly_stream_chemistry.csv',
 #               stringsAsFactors = FALSE)
 # table(nchar(zz2$datetime))
+
+
+# inspect date, time, datetime discrepancies (one time only) ####
+
+zz = p[which(apply(p[, c('date', 'timeEST', 'datetime')], 1,
+                   function(x) sum(is.na(x)) == 2)), c('date', 'timeEST', 'datetime')]
+as.data.frame(zz)
+zzz = s[which(apply(s[, c('date', 'timeEST', 'datetime')], 1,
+                    function(x) sum(is.na(x)) == 2)), c('date', 'timeEST', 'datetime')]
+as.data.frame(zzz)
+
+table(nchar(p$datetime))
+table(nchar(s$datetime))
+
+nrow(p)
+sum(is.na(p$datetime))
+sum(is.na(p$date))
+sum(is.na(p$timeEST))
+nrow(s)
+sum(is.na(s$datetime))
+sum(is.na(s$date))
+sum(is.na(s$timeEST))
+identical(which(is.na(p$timeEST)) , which(is.na(p$datetime)))
+identical(which(is.na(s$timeEST)) , which(is.na(s$datetime)))
+p[is.na(p$timeEST),]$date
+s[is.na(s$timeEST),]$date
+
+qq = bind_rows(p, s)
+nrow(qq)
+sum(is.na(qq$datetime))
+sum(is.na(qq$date))
+sum(is.na(qq$timeEST))
